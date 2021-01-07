3803f29bfb526af01d7958aea25ca67a258cc0e7
Drew.Slone <d.slone317@gmail.com>
2021-01-07 10:48:31 -0800

    Create Program.cs

---
+++ Insurance_Approval_Console_App/Insurance_Approval_Console_App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Insurance_Approval_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("What is your age?");
+            int applicant_Age = Convert.ToInt32(Console.ReadLine());
+            //*CURIOUS* Try to implement 'Yes' and 'No' responses if possible
+            Console.WriteLine("Have you ever had a DUI?  Please respond with 'True' or 'False'.");
+            bool have_DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int num_Tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            if (applicant_Age > 15 && have_DUI == false && num_Tickets <= 3)
+                Console.WriteLine("True");
+            else
+                Console.WriteLine("False");
+            Console.ReadLine();
+        }
+    }
+}
