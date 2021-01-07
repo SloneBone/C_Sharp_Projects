df622024af33ebd5630efeff69beaf0979542cdb
Drew.Slone <d.slone317@gmail.com>
2021-01-07 13:09:19 -0800

    Create Program.cs

---
+++ Shipping_Cost_Console_App/Shipping_Cost_Console_App/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping_Cost_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int ship_Weight = Convert.ToInt32(Console.ReadLine());
+
+            if (ship_Weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day!");
+                Console.WriteLine("Closing Program");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+                
+            }
+           
+
+            Console.WriteLine("Please enter the package width:");
+            int ship_Width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height:");
+            int ship_Height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package length:");
+            int ship_Length = Convert.ToInt32(Console.ReadLine());
+            int ship_dimensions = (ship_Width + ship_Length + ship_Height);
+
+            if (ship_dimensions > 50)
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express.");
+                Console.WriteLine("Closing Program.");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+
+            
+
+            
+            double ship_Cost = Convert.ToDouble((ship_Height * ship_Width * ship_Length) * ship_Weight) / 100;
+            Console.WriteLine("Your estimated shipping cost for this package: {0:C2}", ship_Cost);
+            Console.WriteLine("Thank You");
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
