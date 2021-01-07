1f312c522fdd50d0c244f796f1e72ddff819de13
Drew.Slone <d.slone317@gmail.com>
2021-01-07 10:47:44 -0800

    Create Program.cs

---
+++ Boolean_Logic/Boolean_Logic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //and operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //or operator
+            //Console.WriteLine(false || false);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(true || true);
+
+            //equal to operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //not equal to operatorr
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //xor operator
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.WriteLine(true && true && true && true || false );
+
+            Console.ReadLine();
+        }
+    }
+}
