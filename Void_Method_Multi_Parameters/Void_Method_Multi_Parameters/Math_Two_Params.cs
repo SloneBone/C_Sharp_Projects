511a4b6c87970cce194f5fe269c64d143994a888
Drew.Slone <d.slone317@gmail.com>
2021-01-28 15:44:36 -0800

    Create Math_Two_Params.cs

---
+++ Void_Method_Multi_Parameters/Void_Method_Multi_Parameters/Math_Two_Params.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method_Multi_Parameters
+{
+    class Math_Two_Params
+    {
+        public void TwoParams(int num1, int num2)
+        {
+            _ = num1 / 2;
+
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
