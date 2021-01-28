dc1fea3fe0c4a936926eb45784854b33fd9c3d93
Drew.Slone <d.slone317@gmail.com>
2021-01-28 09:28:48 -0800

    Create MathFunctions.cs

---
+++ More_Class_Methods/More_Class_Methods/MathFunctions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Class_Methods
+{
+    class MathFunctions
+    {
+        public int DoSomeMath(int integer_Input)
+        {
+            int int_Result = integer_Input + 256;
+            return int_Result;
+        }
+        public decimal DoSomeMath(decimal decimal_Input)
+        {
+            decimal dec_Result = decimal_Input * 100;
+            return dec_Result;
+        }
+        public int DoSomeMath(string str_Input)
+        {
+            string intString = str_Input;
+            int i = 0;
+            if (!Int32.TryParse(intString, out i))
+            {
+                i = -1;
+            }
+            return i;
+            
+        }
+
+            
+
+    }
+}
