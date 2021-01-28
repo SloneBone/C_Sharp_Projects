1f2ae5bec5caf852030138d74b770f4feea68934
Drew.Slone <d.slone317@gmail.com>
2021-01-28 14:54:00 -0800

    Create Math_Multi_Parameter.cs

---
+++ Multi_Parameters_Class/Multi_Parameters_Class/Math_Multi_Parameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Parameters_Class
+{
+    class Math_Multi_Parameter
+    {
+        public int DoSomeMath (int input1, int optionalint = 7)
+      
+        {
+            int result = input1 + optionalint;
+
+            return result;
+            
+        }
+    }
+}
