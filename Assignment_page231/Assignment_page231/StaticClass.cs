650e808303dd945eed7ddc8d2d10ae5e87eb475a
Drew.Slone <d.slone317@gmail.com>
2021-01-28 20:25:05 -0800

    Create StaticClass.cs

---
+++ Assignment_page231/Assignment_page231/StaticClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_page231
+{
+    
+    public static class StaticClass
+    {
+        public static string dinner = "Would you look at the time..I gotta go eat dinner now!";
+
+        public static void Whats_For_Dinner()
+        {
+            Console.WriteLine("Turkey Burger!");
+        }
+
+    }
+}
