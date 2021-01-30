071c5d69e4d9ac9ddb29699a217f0de00d4e9dbd
Drew.Slone <d.slone317@gmail.com>
2021-01-29 19:31:35 -0800

    Create Employee.cs

---
+++ Assignment_Interface/Assignment_Interface/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Interface
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit()
+        {
+            
+            Console.WriteLine($"Two week notice of resignation recieved for employee with Id number: {Id}");
+            SayName();
+            Console.WriteLine("\nBegin termination process...");
+            Console.ReadLine();
+        }
+
+    }
+}
