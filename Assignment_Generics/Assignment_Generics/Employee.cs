3d3efea7376321ea83f9f146b20042cab1a7d328
Drew.Slone <d.slone317@gmail.com>
2021-02-01 22:35:52 -0800

    Create Employee.cs

---
+++ Assignment_Generics/Assignment_Generics/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Generics
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit()
+        {
+
+            Console.WriteLine($"Two week notice of resignation recieved for employee with Id number: {Id}");
+            SayName();
+            Console.WriteLine("\nBegin termination process...");
+            Console.ReadLine();
+        }
+        public virtual void GetId()
+        {
+            Console.WriteLine($"Employee Id: {Id}");
+        }
+        //Note
+
+        //The comparison operators must be overloaded in pairs.
+        //That is, if either operator of a pair is overloaded, 
+        //the other operator must be overloaded as well.
+        //Such pairs are as follows:
+
+        //== and != operators
+        //< and > operators
+        //<= and >= operators
+
+        //public static bool operator ==(Employee Id1, Employee Id2)
+        //{
+        //    bool status = false;
+        //    if (Id1.Id == Id2.Id)
+        //    {
+        //        status = true;
+        //    }
+        //    return status;
+        //}
+
+        //public static bool operator !=(Employee Id1, Employee Id2)
+        //{
+        //    bool status = false;
+        //    if (Id1.Id == Id2.Id)
+        //    {
+        //        status = true;
+        //    }
+        //    return status;
+        //}
+    }
+
+}
