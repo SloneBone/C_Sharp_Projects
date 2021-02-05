4f876b97ed96f15db65de90da5394ac13aed847d
Drew.Slone <d.slone317@gmail.com>
2021-02-05 11:22:45 -0800

    Create Program.cs

---
+++ Exception_Handling_2/Exception_Handling_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Perform these actions and create a console app that includes the following:
+
+            //    1.Ask the user for their age.
+            //    2.Display the year the user was born.
+            //    3.Exceptions must be handled using “try/catch.”
+            try
+            {
+                Console.WriteLine("Please enter your age: ");
+                int Age = Convert.ToInt32(Console.ReadLine());
+                DateTime yearOfBirth = DateTime.Now.AddYears(-Age);
+                if (Age <= 0)
+                {
+                    throw new CustomException();
+                }
+
+                Console.WriteLine($"According to my calculations...\nYou were born in the year of: {yearOfBirth.Year}");
+                Console.ReadLine();
+            }
+            //    4.Display appropriate error messages if the user enters zero or negative numbers.
+            //    5.Display a general message if an exception was caused by anything else
+            catch (FormatException)
+            {
+                Console.WriteLine("You have entered something incorrectly...\nTry entering a number!");
+                Console.ReadLine();
+                return;
+            }
+            catch (CustomException)
+            {
+                Console.WriteLine("You have entered something incorrectly...\nPlease enter a whole number greater than zero!");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong....");
+                Console.ReadLine();
+            }
+            finally
+            {
+                Console.WriteLine("The try/catch block was executed!");
+                Console.ReadLine();
+            }
+
+
+
+            //Console.WriteLine("Please enter your age: ");
+            //int Age = Convert.ToInt32(Console.ReadLine());
+            //DateTime yearOfBirth = DateTime.Now.AddYears(-Age);
+
+            //Console.WriteLine($"According to my calculations you were born in the year of: {yearOfBirth.Year}");
+            //Console.ReadLine();
+
+            //DateTime userAge = Convert.ToDateTime(Console.ReadLine());
+            //var birthYear = DateTime.Today.Subtract(userAge);
+            //Console.WriteLine(birthYear);
+
+
+
+        }
+    }
+}
