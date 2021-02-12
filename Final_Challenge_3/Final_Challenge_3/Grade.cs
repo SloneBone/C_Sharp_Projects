2cf7822f1d836114c2c2ff9900eaff544a41a432
Drew.Slone <d.slone317@gmail.com>
2021-02-12 14:28:39 -0800

    Create Grade.cs

---
+++ Final_Challenge_3/Final_Challenge_3/Grade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Challenge_3
+{
+     public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
