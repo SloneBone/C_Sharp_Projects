110d40d547346e55e508fc8cc62c20367df0c1eb
Drew.Slone <d.slone317@gmail.com>
2021-02-12 14:29:45 -0800

    Create Program.cs

---
+++ Final_Challenge_3/Final_Challenge_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Final_Challenge_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill"  };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Lets Check the dB!");
+            Console.WriteLine("Success!");
+
+        }
+    }
+}
